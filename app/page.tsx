9f63aa479e77c1fdf55db6834bd086f2a6c675ce
Dharani-Mediwave <dharani@mindwaveventures.com>
2023-06-09 13:55:22 +0530

    Added readme file

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="">
-      <h1>Welcome to Next.js</h1>
+      <div className='flex justify-center px-8'>
+        <p>DummyJSON can be used with any type of front end project that needs products, carts, users, todos or any dummy data in JSON format. You can use examples below to check how DummyJSON works. Feel free to enjoy it in your awesome projects!</p>
+      </div>
     </main>
   )
 }
